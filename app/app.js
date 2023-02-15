b064ca2ef2cef153f9a8e60b5af8e421d4a38e44
LeeDuho <dl6874@gmail.com>
2023-02-15 21:36:47 +0900

    프론트 단 자바스크립트 파일 추가

--- app/app.js
+++ app/app.js
@@ -12,6 +12,10 @@ const home = require("./src/routes/home"); //index.js를 가져온다.
 app.set("views", "./src/views");//view 폴더를 지정
 app.set("view engine", "ejs");//view engine을 ejs로 설정
 
+app.use(express.static("${__dirname}/src/public"));
+//정적인 파일들을 제공해주는 미들웨어, public 폴더를 제공해준다.
+//만약 login.ejs에서 /js/home.js를 불러오면, public/js/home.js를 불러온다.
+
 app.use("/", home); //미들웨어를 등록해주는 메소드
 
 module.exports = app;
