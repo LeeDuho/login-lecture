e93ba9ba6bc0a74bc440393815e901f2a134b2f2
LeeDuho <dl6874@gmail.com>
2023-02-17 18:10:52 +0900

    루트 src삭제

--- src/bin/www.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const app = require('../app');
-const PORT = 3000;
-
-//listen 명령어를 통해 3000번 포트를 열어준다.
-app.listen(PORT, () => {
-  console.log('Example app listening on port 3000!');
-});
